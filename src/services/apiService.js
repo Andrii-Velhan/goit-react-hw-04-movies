368f19ad2756d25efb1998000e2687ace160a841
Andrii-Velhan <velganaa@gmail.com>
2021-02-12 21:08:55 +0200

    add api url

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,7 @@
 const BASE_URL = `https://api.themoviedb.org/3/`;
 const MY_KEY = `66acb4573ba980ae8ac5981a52e8de6b`;
 // const PER_PAGE = 12;
+// const APIaccessKeyV4auth = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmFjYjQ1NzNiYTk4MGFlOGFjNTk4MWE1MmU4ZGU2YiIsInN1YiI6IjYwMjY5MjZiZDZjMzAwMDA0MTcwYjQ1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VfVRytfR82QgUddj6bVVXNcEbxcj7_D2z_leUhvgTds`;
 
 export function fetchImg(page, query) {
   const url = `${BASE_URL}movie/550?api_key=${MY_KEY}`;
